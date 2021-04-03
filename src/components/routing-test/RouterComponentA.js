9fe0f097cf4c20430ebcf45013d1c818f5a98c2d
m2507 <mohitsaxena2507@gmail.com>
2021-04-03 17:05:38 +0530

    added Helper common class and util file

--- src/components/routing-test/RouterComponentA.js
+++ src/components/routing-test/RouterComponentA.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
+import Helper from "./../../util/Helper";
+import { addTwoNumber, getUserName } from "./../../util/util";
 
 const RouterComponentA = (props) => {
+  const [user, setUser] = useState({});
 
-    useEffect(() => {
-        const params = props.match;
-        console.table(params);
-      
-    }, [props.match]);
+  useEffect(() => {
+    Helper.getPromiseData().then((res) => {
+      setUser(res);
+    });
+    const params = props.match;
+    console.table(params);
+  }, [props.match]);
   /**
    * Router Navigation is done using props.history
    * See RouterComponentB for other way to route using hook
@@ -15,13 +20,20 @@ const RouterComponentA = (props) => {
   const onClick = () => {
     props.history.push("/routerB");
   };
- 
 
   return (
     <>
-      <h1>Welcome to Router Component A</h1>
+      <h3>Welcome to Router Component A</h3>
+      <h6>Called util function: Username is {getUserName()}</h6>
+      <h6>Sum of two number (3,4) using util.js - {addTwoNumber(3, 4)}</h6>
+      <h6>Sum of two number (8,4) using Helper.js - {Helper.printSum(8, 4)}</h6>
+      <h6>
+        Multiply of two number (8,4) using Helper.js - {Helper.multiply(8, 4)}
+      </h6>
+      <h6>Below data is coming through Promise Resolve from Helper class</h6>
+      <p>User: {user.name}</p>
+      <p>Id: {user.id}</p>
       <Button onClick={onClick}>Route to B</Button>
-     
     </>
   );
 };
